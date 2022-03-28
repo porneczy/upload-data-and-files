3325c6049828c1c8a195c9785f467bde73d2e0f7
porneczy <porneczy@gmail.com>
2022-03-28 15:13:38 +0200

    beforeFeladat

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload")
 const path = require("path");
 const app = express();
 
@@ -6,8 +7,11 @@ function getFunction(request, response){
     response.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 }
 
+app.use(fileUpload());
+
 app.get("/", getFunction);
 app.use("/pub", express.static(`${__dirname}/../frontend/public`));
+app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
 
 const port = 9000;
 const ipAddress = `http://127.0.0.1:${port}`;
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,4 +1,41 @@
+const formComponent = `
+    <form id="form">
+        <input type="text" name="title">
+        <input type="file" name="picture">
+        <button>Send</button>
+    </form>
+`
+
 function loadEvent() {
-    console.log("hello");
+    const rootElement = document.getElementById("root")
+
+    rootElement.insertAdjacentHTML("beforeend", formComponent)
+
+    const formElement = document.getElementById("form")
+    formElement.addEventListener("submit", e => {
+        e.preventDefault()
+        
+        const formData = new FormData()// ez egy doboz. ezt postázzuk és ebbe pakolunk dolgokat amit el akarunk küldeni
+        formData.append("title", e.target.querrySelector(`input[name="title"]`).value)
+        formData.append("picture", e.target.querrySelector(`input[name="picture"]`).files[0])
+        //itt belepakoltuk amit el akarunk küldeni
+        
+        const fetchSettings = {
+            method: "POST",
+            body: formData
+        }
+        fetch("/", fetchSettings)
+            .then(data => {
+                if (data.status === 200) {
+                    e.target.outerHTML = "done"
+                    console.dir(data);
+                }  
+            })
+            .catch(error => {
+                e.target.outerHTML = "error"
+                console.dir(error);
+            })
+    })
 }
+
 window.addEventListener("load", loadEvent)
